bb8cea450be42f45544beea44e02d4660b418247
Jesper Rosholm Tørresø <jrt@ase.au.dk>
2020-11-04 08:38:50 +0100

    MaterialType tilføjet den generelle entitet Material

--- StudyNoteAppDB/dbo/Tables/Material.sql
+++ StudyNoteAppDB/dbo/Tables/Material.sql
@@ -6,7 +6,8 @@
 CREATE TABLE Material (
     MaterialId     BIGINT IDENTITY(1,1) NOT NULL,
     PublisheId     BIGINT NOT NULL,
-CONSTRAINT pk_Material PRIMARY KEY CLUSTERED (MaterialId),
+[MaterialType] NVARCHAR(50) NULL, 
+    CONSTRAINT pk_Material PRIMARY KEY CLUSTERED (MaterialId),
 CONSTRAINT fk_Material FOREIGN KEY (PublisheId)
     REFERENCES Publisher (PublisheId)
     ON DELETE NO ACTION
